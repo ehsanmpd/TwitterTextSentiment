3ede3697d05a93dc2e5a6dea9fd2bcf3be21ec0b
Ehsan Mohammadpour <ehsan.mohammadpour@epfl.ch>
2019-12-03 10:19:30 +0100

    dataset added

---
+++ run.py
@@ -0,0 +1,9 @@
+""" implemetation of all steps to run the project in sequential"""
+""" Authors: Mohammad, Ehsan, Fereshte """
+""" Date: 10 November 2019 """
+
+import numpy as np
+import sys
+from helpers import *
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
